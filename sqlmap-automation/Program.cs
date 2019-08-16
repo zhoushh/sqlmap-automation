19fa15b0e72bd4617bf0be244c65b228c48429fa
Shihao "Steven" Zhou <dont_email_me@example.com>
2019-08-16 14:16:40 +0800

    This is a test Main method to test if session class works fine.

--- sqlmap-automation/Program.cs
+++ sqlmap-automation/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace sqlmap_automation
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string host = args[0];
+			int port = int.Parse(args[1]);
+			using (SqlmapSession session = new SqlmapSession(host, port))
+			{
+				string response = session.ExecuteGet("/task/new");
+				JToken token = JObject.Parse(response);
+				string taskId = token.SelectToken("taskid").ToString();
+
+				Console.WriteLine("New task ID: " + taskId);
+				Console.WriteLine("Deleting task:" + taskId);
+
+				response = session.ExecuteGet("/task/" + taskId + "/delete/");
+				token = JObject.Parse(response);
+				bool isSuccessful = (bool) token.SelectToken("success");
+
+				Console.WriteLine("delete successful: " + isSuccessful);
+			}
+		}
+	}
 }
